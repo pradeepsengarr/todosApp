cb605af04bb4ef57d8e6809330f1e63ff31b1743
pradeep singh sengar <pradeep19sengar@gmail.com>
2021-10-20 21:39:18 +0530

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is the simple todo_List
+            <p>This is created with the help of react ja and frameworks
+            </p>
+                    </div>
+    )
+}
